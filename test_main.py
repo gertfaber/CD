70331b446abc82e7dc1114bc06dae548781ea7ef
Gert Faber <gertfaber@gmail.com>
2023-06-02 21:05:45 +0200

    remove error

--- test_main.py
+++ test_main.py
@@ -13,4 +13,4 @@ def client():
 def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello, world test2e!"
+    assert response.data == b"Hello, world test2!"
