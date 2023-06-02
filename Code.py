ac8bfca1ee6b3b1146505eab1ab546610c03abef
Gert Faber <gertfaber@gmail.com>
2023-06-02 15:44:48 +0200

    15

--- Code.py
+++ Code.py
@@ -3,7 +3,7 @@ git add . #(all  files)
 git status
 git commit -m 'commit 12'
 git remote add origin https://github.com/gertfaber/CD.git
-git push -u origin master
+git push -u origin master 
 
 
 git clone https://github.com/gertfaber/CD.git
