cfd80d4226e97c65eeda99b9d581713d9e667840
Gert Faber <gertfaber@gmail.com>
2023-06-02 14:58:12 +0200

    new 12

--- Code.py
+++ Code.py
@@ -8,3 +8,7 @@ git push -u origin master
 
 git clone https://github.com/gertfaber/CD.git
 git pull https://github.com/gertfaber/CD.git
+
+
+git remote add origin https://github.com/gertfaber/Drop.git
+
