37c412d609c9426510a40aa72ad7f6ae95a491f7
Gert Faber <gertfaber@gmail.com>
2023-06-02 13:03:03 +0200

    commit 2

--- Code.py
+++ Code.py
@@ -1,8 +1,10 @@
 git init
 git add . #(all  files)
 git status
-git commit -m 'commit 1'
+git commit -m 'commit 2'
 git remote add origin https://github.com/gertfaber/CD.git
 git push -u origin master
 
+
+git clone https://github.com/gertfaber/CD.git
 
