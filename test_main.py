b503d9e2c7a455c7889e91b2b55f9ad9d32e5b05
Gert Faber <gertfaber@gmail.com>
2023-06-02 20:56:31 +0200

    pytest

---
+++ test_main.py
@@ -0,0 +1,16 @@
+import pytest
+
+from farm.main import app
+
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hello, world test2!"
